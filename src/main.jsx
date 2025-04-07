95b6efa85b008ae63e673a28619207d9cbb13eb8
KW316 <kiannamarie.wilson@cdpipelinedevshop.com>
2025-04-07 16:18:59 -0400

    one

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ConsumerSubscription from './assets/consumer-subscription.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ConsumerSubscription />
   </StrictMode>,
 )
